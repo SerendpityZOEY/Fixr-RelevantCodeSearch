7bd3819ab02b8ad012db606e6069ba6e47385c9a
SerendpityZOEY <zoey.zhang513@hotmail.com>
2017-01-18 00:02:14 -0700

    add entry for page2 & method highlight done

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  /*
   entry: [
     './lib/src/app.js',
   ],
+  */
+  entry:{
+    'page1': './lib/src/app.js',
+    'page2': './lib/src/page2/main.js'
+  },
   output: {
     path: path.join(__dirname, './public/js/'),
-    filename: 'bundle.js',
+    filename: '[name].js',
     publicPath: '/js/'
   },
   module: {
@@ -16,6 +23,23 @@ module.exports = {
       loader: 'babel',
       query: { presets: ['react', 'es2015'],
         plugins: ['recharts']}
-    }]
-  }
+    },
+      {
+        test: /\.css$/,
+        loader: 'style!css?modules',
+        include: /flexboxgrid/,
+      }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      chunks: ['page1'],
+      filename: 'public/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      chunks: ['page2'],
+      filename: 'public/test.html'
+    })
+  ]
 };
